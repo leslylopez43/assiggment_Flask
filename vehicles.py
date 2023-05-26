0d7997705a2e64b1f1ac36faa6ad07d6b46a1b4b
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-05-26 15:00:14 +0100

    try to running

---
+++ vehicles.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn=psycopg2.connect(database="motoring",host="localhost",user="postgres",password="London1031",port="5432")
+
+cur=conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS vehicles(VehiclesID serial PRIMARY KEY, registration_number varchar(100), brand varchar(100), model varchar(100), color varchar(100), Price money, car_year varchar(100), On_stock_from varchar(100), available varchar(100);''')
+#sql_string="INSERT INTO vehicles(Registracion_number, Brand, Model, Color, price, Car_year, On_stock_From, Available) VALUES ('MC23URD','Mercedez', 'Benz', 'White', '£2020', '2023', '23/02/2023', 'yes');"
+
+#cur.execute(sql_string)
+cur.execute('''INSERT INTO vehicles(registration_number, brand, model, color, price_name, car_year, on_stock_from, available) VALUES ('bre12wef','Mercedes', 'Benz', 'white', '£10233','2023', '20/02/2033', 'yes';''') # imperial@imperial.com, 40 Bounder roar unit 5 N15 9HI, London),('supplier',6000,30);''')
+
+
+conn.commit()
+
+cur.close()
+
+conn.close()
+
