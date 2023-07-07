d123a18e03e68b8bda0b6a0eb01597510c60c7f2
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-07-07 12:02:45 +0100

    update the password
    to connect to data base

--- Db2_App.py
+++ Db2_App.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, redirect, request, url_for
 import psycopg2
 app=Flask(__name__)
 def db_conn():
-    conn=psycopg2.connect(database="motoring",host="localhost",user="postgres",password="London1031",port="5432")
+    conn=psycopg2.connect(database="motoring",
+    host="dpg-cijtiih8g3nc2ge601gg-a", 
+    user="postgres",
+    password="an0qLg5cQMuA6gyATcsElx0L1srvkvGb",
+    port="5432")
     return conn
 
 
